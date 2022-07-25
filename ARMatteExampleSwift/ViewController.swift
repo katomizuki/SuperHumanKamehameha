d66444da33e38a524d35235497b0f17d90934727
Katopan <76249494+katomizuki@users.noreply.github.com>
2022-07-26 07:49:10 +0900

    lighteffect

--- ARMatteExampleSwift/ViewController.swift
+++ ARMatteExampleSwift/ViewController.swift
@@ -15,100 +15,63 @@ extension MTKView: RenderDestinationProvider {
 
 class ViewController: UIViewController, MTKViewDelegate, ARSessionDelegate {
     
-    var session: ARSession!
+//    var session: ARSession!
+//    var renderer: Renderer!
+    
+    var session = ARSession()
     var renderer: Renderer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
-        session = ARSession()
-        session.delegate = self
-        
-        // Set the view to use the default device
         if let view = self.view as? MTKView {
             view.device = MTLCreateSystemDefaultDevice()
             view.backgroundColor = UIColor.clear
             view.delegate = self
-            
-            guard view.device != nil else {
-                print("Metal is not supported on this device")
-                return
-            }
-            
-            // Configure the renderer to draw to the view
-            renderer = Renderer(session: session, metalDevice: view.device!, renderDestination: view)
-            
+            print("ああああああああああ")
+            renderer = Renderer(session: session, metalDevice: view.device!, mtkView: view)
             renderer.drawRectResized(size: view.bounds.size)
         }
-        
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.handleTap(gestureRecognize:)))
-        view.addGestureRecognizer(tapGesture)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
-        // Enable frame semantics
-        // .personSegmentation or .personSegmentationWithDepth
-        configuration.frameSemantics = .personSegmentationWithDepth
-
-        // Run the view's session
+        configuration.frameSemantics = .personSegmentation
         session.run(configuration)
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Pause the view's session
-        session.pause()
-    }
-    
-    @objc
-    func handleTap(gestureRecognize: UITapGestureRecognizer) {
-        // Create anchor using the camera's current position
-        if let currentFrame = session.currentFrame {
-            
-            // Create a transform with a translation of 1.0 meters in front of the camera
-            var translation = matrix_identity_float4x4
-            translation.columns.3.z = -1.0
-            let transform = simd_mul(currentFrame.camera.transform, translation)
-            
-            // Add a new anchor to the session
-            let anchor = ARAnchor(transform: transform)
-            session.add(anchor: anchor)
-        }
-    }
-    
-    // MARK: - MTKViewDelegate
-    
-    // Called whenever view changes orientation or layout is changed
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        print("いいいいいいいいああ")
         renderer.drawRectResized(size: size)
     }
     
-    // Called whenever the view needs to render
     func draw(in view: MTKView) {
+        print("うううううううう")
         renderer.update()
     }
     
-    // MARK: - ARSessionDelegate
-    
+//
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        // Pause the view's session
+        session.pause()
+    }
+
+//
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
-        
+
     }
-    
+
     func sessionWasInterrupted(_ session: ARSession) {
         // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
+
     }
-    
+
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+
     }
 }
